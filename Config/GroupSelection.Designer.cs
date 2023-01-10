6e7d387024f0535b0b06beb66fc0bd4a8bc88430
Phillip-Smelt <phillip.smelt@borisch.com>
2023-01-10 13:25:19 -0500

    1/10/2023

--- Config/GroupSelection.Designer.cs
+++ Config/GroupSelection.Designer.cs
@@ -24,16 +24,19 @@ namespace TestLibrary.Config {
             this.LabelGroups = new System.Windows.Forms.Label();
             this.OK = new System.Windows.Forms.Button();
             this.ListGroups = new System.Windows.Forms.ListView();
+            this.radioButtonRequired = new System.Windows.Forms.RadioButton();
+            this.radioButtonNotRequired = new System.Windows.Forms.RadioButton();
+            this.groupBoxRequired = new System.Windows.Forms.GroupBox();
+            this.groupBoxRequired.SuspendLayout();
             this.SuspendLayout();
             // 
             // LabelGroups
             // 
             this.LabelGroups.AutoSize = true;
             this.LabelGroups.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.LabelGroups.Location = new System.Drawing.Point(16, 13);
-            this.LabelGroups.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LabelGroups.Location = new System.Drawing.Point(16, 7);
             this.LabelGroups.Name = "LabelGroups";
-            this.LabelGroups.Size = new System.Drawing.Size(65, 13);
+            this.LabelGroups.Size = new System.Drawing.Size(81, 16);
             this.LabelGroups.TabIndex = 0;
             this.LabelGroups.Text = "Test Groups";
             this.LabelGroups.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -42,11 +45,11 @@ namespace TestLibrary.Config {
             // 
             this.OK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.OK.Enabled = false;
-            this.OK.Location = new System.Drawing.Point(169, 298);
-            this.OK.Margin = new System.Windows.Forms.Padding(2);
+            this.OK.Location = new System.Drawing.Point(225, 367);
+            this.OK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.OK.Name = "OK";
-            this.OK.Size = new System.Drawing.Size(58, 36);
-            this.OK.TabIndex = 1;
+            this.OK.Size = new System.Drawing.Size(77, 44);
+            this.OK.TabIndex = 4;
             this.OK.Text = "OK";
             this.OK.UseVisualStyleBackColor = true;
             this.OK.Click += new System.EventHandler(this.OK_Click);
@@ -57,32 +60,70 @@ namespace TestLibrary.Config {
             this.ListGroups.GridLines = true;
             this.ListGroups.HideSelection = false;
             this.ListGroups.LabelWrap = false;
-            this.ListGroups.Location = new System.Drawing.Point(12, 39);
+            this.ListGroups.Location = new System.Drawing.Point(13, 27);
+            this.ListGroups.Margin = new System.Windows.Forms.Padding(4);
             this.ListGroups.MultiSelect = false;
             this.ListGroups.Name = "ListGroups";
             this.ListGroups.ShowGroups = false;
-            this.ListGroups.Size = new System.Drawing.Size(366, 254);
+            this.ListGroups.Size = new System.Drawing.Size(487, 312);
             this.ListGroups.TabIndex = 0;
             this.ListGroups.UseCompatibleStateImageBehavior = false;
             this.ListGroups.View = System.Windows.Forms.View.Details;
             this.ListGroups.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ListGroups_SelectionChanged);
             // 
+            // radioButtonRequired
+            // 
+            this.radioButtonRequired.AutoSize = true;
+            this.radioButtonRequired.Location = new System.Drawing.Point(0, 18);
+            this.radioButtonRequired.Name = "radioButtonRequired";
+            this.radioButtonRequired.Size = new System.Drawing.Size(84, 20);
+            this.radioButtonRequired.TabIndex = 2;
+            this.radioButtonRequired.TabStop = true;
+            this.radioButtonRequired.Text = "Required";
+            this.radioButtonRequired.UseVisualStyleBackColor = true;
+            this.radioButtonRequired.CheckedChanged += new System.EventHandler(this.groupBoxRequired_CheckedChanged);
+            // 
+            // radioButtonNotRequired
+            // 
+            this.radioButtonNotRequired.AutoSize = true;
+            this.radioButtonNotRequired.Location = new System.Drawing.Point(1, 44);
+            this.radioButtonNotRequired.Name = "radioButtonNotRequired";
+            this.radioButtonNotRequired.Size = new System.Drawing.Size(108, 20);
+            this.radioButtonNotRequired.TabIndex = 3;
+            this.radioButtonNotRequired.TabStop = true;
+            this.radioButtonNotRequired.Text = "Not Required";
+            this.radioButtonNotRequired.UseVisualStyleBackColor = true;
+            this.radioButtonNotRequired.CheckedChanged += new System.EventHandler(this.groupBoxRequired_CheckedChanged);
+            // 
+            // groupBoxRequired
+            // 
+            this.groupBoxRequired.Controls.Add(this.radioButtonNotRequired);
+            this.groupBoxRequired.Controls.Add(this.radioButtonRequired);
+            this.groupBoxRequired.Location = new System.Drawing.Point(12, 346);
+            this.groupBoxRequired.Name = "groupBoxRequired";
+            this.groupBoxRequired.Size = new System.Drawing.Size(121, 84);
+            this.groupBoxRequired.TabIndex = 1;
+            this.groupBoxRequired.TabStop = false;
+            // 
             // GroupSelect
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 339);
+            this.ClientSize = new System.Drawing.Size(525, 457);
             this.ControlBox = false;
+            this.Controls.Add(this.groupBoxRequired);
             this.Controls.Add(this.ListGroups);
             this.Controls.Add(this.OK);
             this.Controls.Add(this.LabelGroups);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "GroupSelect";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Choose Test Group";
+            this.groupBoxRequired.ResumeLayout(false);
+            this.groupBoxRequired.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,5 +132,8 @@ namespace TestLibrary.Config {
         private Label LabelGroups;
         private Button OK;
         private ListView ListGroups;
+        private RadioButton radioButtonRequired;
+        private RadioButton radioButtonNotRequired;
+        private GroupBox groupBoxRequired;
     }
 }
--- Config/GroupSelection.cs
+++ Config/GroupSelection.cs
@@ -1,29 +1,50 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace TestLibrary.Config {
     public partial class GroupSelect : Form {
         public String GroupSelected;
+        internal Dictionary<String, Group> _groups { get; private set; }
+        private List<String> _keysRequired;
+        private List<String> _keysNotRequired;
+
         public GroupSelect(Dictionary<String, Group> Groups) {
-            InitializeComponent();
+            if (Groups.Count() == 0) throw new ArgumentNullException($"Dictionary 'Groups' is empty.");
+            this.InitializeComponent();
+            this._groups = Groups;
+            this._keysRequired = this._groups.Where(g => (g.Value.Required)).Select(g => g.Key).ToList();
+            this._keysNotRequired = this._groups.Where(g => (!g.Value.Required)).Select(g => g.Key).ToList();
+            this.ListGroups.MultiSelect = false;
+
+            this.ListViewRefresh();
+
+            this.radioButtonRequired.Enabled = (this._keysRequired.Count > 0); // Set radioButtonRequired to default if there are Required Groups.
+            this.radioButtonNotRequired.Enabled = (this._keysNotRequired.Count > 0);
+
+            this.FormRefresh();
+        }
+
+        private void ListViewRefresh() {
+            this.ListGroups.Clear();
             this.ListGroups.View = View.Details;
             this.ListGroups.Columns.Add("ID");
-            this.ListGroups.Columns.Add("Required?");
+            // this.ListGroups.Columns.Add("Required?");
             this.ListGroups.Columns.Add("Summary");
-            String[] item = new String[3];
-            foreach (KeyValuePair<String, Group> kvp in Groups) {
-                item[0] = kvp.Value.ID;
-                item[1] = kvp.Value.Required.ToString();
-                item[2] = kvp.Value.Summary;
-                this.ListGroups.Items.Add(new ListViewItem(item));
-            }
+        }
+
+        private void FormRefresh() {
+            if (this.radioButtonRequired.Checked) foreach (String key in this._keysRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
+            else if (this.radioButtonNotRequired.Checked) foreach (String key in this._keysNotRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
             this.ListGroups.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent);
-            this.ListGroups.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.HeaderSize);
-            this.ListGroups.Columns[2].Width = -2;
+            // this.ListGroups.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.HeaderSize);
+            this.ListGroups.Columns[1].Width = -2;
             // https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.columnheader.width?redirectedfrom=MSDN&view=windowsdesktop-7.0#System_Windows_Forms_ColumnHeader_Width
             this.ListGroups.ResetText();
+            this.ListGroups.Refresh();
             this.OK.Enabled = false;
         }
 
@@ -44,6 +65,19 @@ namespace TestLibrary.Config {
 
         private void ListGroups_SelectionChanged(Object sender, ListViewItemSelectionChangedEventArgs e) {
             this.OK.Enabled = true;
+        }
+
+        private void groupBoxRequired_CheckedChanged(Object sender, EventArgs e) {
+            // Do stuff only if the radio button is checked (or the action will run twice).
+            if (((RadioButton)sender).Checked) {
+                //if (((RadioButton)sender) == this.radioButtonRequired) {
+                //    foreach (String key in this._keysRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
+                //} else if (((RadioButton)sender) == this.radioButtonNotRequired) {
+                //    foreach (String key in this._keysNotRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
+                //}
+                this.ListViewRefresh();
+                this.FormRefresh();
+            }
         }
     }
 }
