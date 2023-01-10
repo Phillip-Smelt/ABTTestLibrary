56f52a64e6323e5bd1a8c13dfa3a55632997cba5
Phillip-Smelt <phillip.smelt@borisch.com>
2023-01-10 13:35:57 -0500

    1/10/2023

--- Config/GroupSelection.cs
+++ Config/GroupSelection.cs
@@ -13,7 +13,6 @@ namespace TestLibrary.Config {
         private List<String> _keysNotRequired;
 
         public GroupSelect(Dictionary<String, Group> Groups) {
-            if (Groups.Count() == 0) throw new ArgumentNullException($"Dictionary 'Groups' is empty.");
             this.InitializeComponent();
             this._groups = Groups;
             this._keysRequired = this._groups.Where(g => (g.Value.Required)).Select(g => g.Key).ToList();
@@ -21,10 +20,8 @@ namespace TestLibrary.Config {
             this.ListGroups.MultiSelect = false;
 
             this.ListViewRefresh();
-
-            this.radioButtonRequired.Enabled = (this._keysRequired.Count > 0); // Set radioButtonRequired to default if there are Required Groups.
+            this.radioButtonRequired.Enabled = (this._keysRequired.Count > 0);
             this.radioButtonNotRequired.Enabled = (this._keysNotRequired.Count > 0);
-
             this.FormRefresh();
         }
 
@@ -32,7 +29,6 @@ namespace TestLibrary.Config {
             this.ListGroups.Clear();
             this.ListGroups.View = View.Details;
             this.ListGroups.Columns.Add("ID");
-            // this.ListGroups.Columns.Add("Required?");
             this.ListGroups.Columns.Add("Summary");
         }
 
@@ -40,11 +36,9 @@ namespace TestLibrary.Config {
             if (this.radioButtonRequired.Checked) foreach (String key in this._keysRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
             else if (this.radioButtonNotRequired.Checked) foreach (String key in this._keysNotRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
             this.ListGroups.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent);
-            // this.ListGroups.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.HeaderSize);
             this.ListGroups.Columns[1].Width = -2;
             // https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.columnheader.width?redirectedfrom=MSDN&view=windowsdesktop-7.0#System_Windows_Forms_ColumnHeader_Width
             this.ListGroups.ResetText();
-            this.ListGroups.Refresh();
             this.OK.Enabled = false;
         }
 
@@ -68,13 +62,7 @@ namespace TestLibrary.Config {
         }
 
         private void groupBoxRequired_CheckedChanged(Object sender, EventArgs e) {
-            // Do stuff only if the radio button is checked (or the action will run twice).
-            if (((RadioButton)sender).Checked) {
-                //if (((RadioButton)sender) == this.radioButtonRequired) {
-                //    foreach (String key in this._keysRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
-                //} else if (((RadioButton)sender) == this.radioButtonNotRequired) {
-                //    foreach (String key in this._keysNotRequired) this.ListGroups.Items.Add(new ListViewItem(new String[] { this._groups[key].ID, this._groups[key].Summary }));
-                //}
+            if (((RadioButton)sender).Checked) { // Do stuff only if the radio button is checked (or the action will run twice).
                 this.ListViewRefresh();
                 this.FormRefresh();
             }
