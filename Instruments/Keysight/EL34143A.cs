bfbc3b364a2273bceecfa39d751ad4f385dd5894
Phillip-Smelt <phillip.smelt@borisch.com>
2023-03-15 09:33:12 -0400

    3/15/2023

--- Instruments/Keysight/EL34143A.cs
+++ Instruments/Keysight/EL34143A.cs
@@ -8,7 +8,6 @@ using Agilent.CommandExpert.ScpiNet.AgEL30000_1_2_5_1_0_6_17_114;
 // Recommend using Command Expert to generate SCPI & IVI drivers commands, which are directly exportable as .Net statements.
 //
 namespace TestLibrary.Instruments.Keysight {
-    // TODO: EL34143A Class.
     public static class EL34143A {
         // NOTE: Consider using IVI driver instead of wrapping SCPI driver's calls.
         public static void Local(Instrument instrument) { ((AgEL30000)instrument.Instance).SCPI.SYSTem.LOCal.Command(); }
@@ -24,10 +23,64 @@ namespace TestLibrary.Instruments.Keysight {
             ((AgEL30000)instrument.Instance).SCPI.CLS.Command();
             ((AgEL30000)instrument.Instance).SCPI.OUTPut.PROTection.CLEar.Command();
             ((AgEL30000)instrument.Instance).SCPI.DISPlay.WINDow.TEXT.CLEar.Command();
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.CURRent.PROTection.STATe.Command(false, null);
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.POWer.PROTection.STATe.Command(false, null);
         }
 
-        public static void On(Instrument instrument, Double ampsDC) {
-            ((AgEL30000)instrument.Instance).SCPI.SOURce.CURRent.LEVel.IMMediate.AMPLitude.Command(ampsDC, null);
+        public static void OnConstantCurrent(Instrument instrument, Double amps) {
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.CURRent.LEVel.IMMediate.AMPLitude.Query("MINimum", null, out Double min);
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.CURRent.LEVel.IMMediate.AMPLitude.Query("MAXimum", null, out Double max);
+            if ((amps < min) || (amps > max)) {
+                String s = $"> MINimum/MAXimum Current.{Environment.NewLine}"
+                    + $" - MINimum   :  Current={min} A.{Environment.NewLine}"
+                    + $" - Programmed:  Current={amps} A.{Environment.NewLine}"
+                    + $" - MAXimum   :  Current={max} A.";
+                throw new InvalidOperationException(InstrumentTasks.GetMessage(instrument, s));
+            }
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.CURRent.LEVel.IMMediate.AMPLitude.Command(amps, null);
+            ((AgEL30000)instrument.Instance).SCPI.OUTPut.STATe.Command(true, null);
+        }
+
+        public static void OnConstantVoltage(Instrument instrument, Double volts) {
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.VOLTage.LEVel.IMMediate.AMPLitude.Query("MINimum", null, out Double[] min);
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.VOLTage.LEVel.IMMediate.AMPLitude.Query("MAXimum", null, out Double[] max);
+            if ((volts < min[0]) || (volts > max[1])) {
+                String s = $"< MINimum/MAXimum Voltage.{Environment.NewLine}"
+                    + $" - MINimum   :  Voltage={min[0]} V.{Environment.NewLine}"
+                    + $" - Programmed:  Voltage={volts} V.{Environment.NewLine}"
+                    + $" - MAXimum   :  Voltage={max[1]} V.";
+                throw new InvalidOperationException(InstrumentTasks.GetMessage(instrument, s));
+            }
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.VOLTage.LEVel.IMMediate.AMPLitude.Command(volts, null);
+            ((AgEL30000)instrument.Instance).SCPI.OUTPut.STATe.Command(true, null);
+        }
+
+        public static void OnConstantPower(Instrument instrument, Double watts) {
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.POWer.LEVel.IMMediate.AMPLitude.Query("MINimum", null, out Double[] min);
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.POWer.LEVel.IMMediate.AMPLitude.Query("MAXimum", null, out Double[] max);
+            if ((watts < min[0]) || (watts > max[1])) {
+                String s = $"< MINimum/MAXimum Wattage.{Environment.NewLine}"
+                    + $" - MINimum   :  Wattage={min[0]} W.{Environment.NewLine}"
+                    + $" - Programmed:  Wattage={watts} W.{Environment.NewLine}"
+                    + $" - MAXimum   :  Wattage={max[1]} W.";
+                throw new InvalidOperationException(InstrumentTasks.GetMessage(instrument, s));
+            }
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.POWer.LEVel.IMMediate.AMPLitude.Command(watts, null);
+            ((AgEL30000)instrument.Instance).SCPI.OUTPut.STATe.Command(true, null);
+        }
+
+        public static void OnConstantResistance(Instrument instrument, Double resistance) {
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.RESistance.LEVel.IMMediate.AMPLitude.Query("MINimum", null, out Double[] min);
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.RESistance.LEVel.IMMediate.AMPLitude.Query("MAXimum", null, out Double[] max);
+            if ((resistance < min[0]) || (resistance > max[1])) {
+                String s = $"< MINimum/MAXimum Resistance.{Environment.NewLine}"
+                    + $" - MINimum   :  Resistance={min[0]} Ω.{Environment.NewLine}"
+                    + $" - Programmed:  Resistance={resistance} Ω.{Environment.NewLine}"
+                    + $" - MAXimum   :  Resistance={max[1]} Ω.";
+                throw new InvalidOperationException(InstrumentTasks.GetMessage(instrument, s));
+            }
+            ((AgEL30000)instrument.Instance).SCPI.SOURce.RESistance.LEVel.IMMediate.AMPLitude.Command(resistance, null);
+            ((AgEL30000)instrument.Instance).SCPI.OUTPut.STATe.Command(true, null);
         }
 
         public static void Off(Instrument instrument) {
