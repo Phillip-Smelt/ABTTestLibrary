0607d532fd6e6b3fdae0d5537e9e6a908b085dc1
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2023-04-11 04:22:14 -0400

    4/11/2023

--- Switching/USB_ERB24.cs
+++ Switching/USB_ERB24.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using MccDaq; // MCC DAQ Universal Library 6.73 from https://www.mccdaq.com/Software-Downloads.
 
 namespace TestLibrary.Switching {
@@ -20,10 +21,7 @@ namespace TestLibrary.Switching {
     //  - Specify USB-ERB24s in App.config, then confirm existence during TestLibrary's initialization.
     // NOTE: MCC's InstaCal USB_ERB24's board number indexing begins at 0, guessing because USB device indexing is likely also zero based.
     // - So ERB24_BOARDS.E01 numerical value is 0, which is used when constructing a new MccBoard ERB24_BOARDS.E01 object:
-    //   - Instantiation 'new MccBoard((Int32)ERB24_BOARDS.E01)' is equivalent to 'new MccBoard(0)'.
-    //   - Wish MCC had zero-indexed their Relays as well; numbering from R0 to R23 instead of R1 to R24.
-    //     - Would've been optimal, as Relays 1 to 24 are controlled by digital port bits that are zero-indexed, from 0 to 23.
-    //     - Because MCC one-indexed their relays from 1 to 24, followed their convention by one-indexing enum ERB24_BOARDS; first USB-ERB24 assigned to E01, not E00.
+    // - Instantiation 'new MccBoard((Int32)ERB24_BOARDS.E01)' is equivalent to 'new MccBoard(0)'.
     public enum ERB24_RELAYS {
         R01 = ERB24_BITS.B00, R02 = ERB24_BITS.B01, R03 = ERB24_BITS.B02, R04 = ERB24_BITS.B03, R05 = ERB24_BITS.B04, R06 = ERB24_BITS.B05, R07 = ERB24_BITS.B06, R08 = ERB24_BITS.B07,
         R09 = ERB24_BITS.B08, R10 = ERB24_BITS.B09, R11 = ERB24_BITS.B10, R12 = ERB24_BITS.B11, R13 = ERB24_BITS.B12, R14 = ERB24_BITS.B13, R15 = ERB24_BITS.B14, R16 = ERB24_BITS.B15,
@@ -33,19 +31,18 @@ namespace TestLibrary.Switching {
     public enum ERB24_BITS {
         B00 = 00, B01 = 01, B02 = 02, B03 = 03, B04 = 04, B05 = 05, B06 = 06, B07 = 07,
         B08 = 08, B09 = 09, B10 = 10, B11 = 11, B12 = 12, B13 = 13, B14 = 14, B15 = 15,
-        B16 = 16, B17 = 17, B18 = 18, B19 = 19, B20 = 20, B21 = 21, B22 = 22, B23 = 23
+        B16 = 16, B17 = 17, B18 = 18, B19 = 19, B20 = 20, B21 = 21, B22 = 22, B23 = 23,
     }
-    // NOTE: USB-ERB24 Relays are divided into 4 configurable groups, controllable via digital Ports A, B, CL & CH.
-    //  - Port A :  Relays 01 - 08.
-    //  - Port B :  Relays 09 - 16.
-    //  - Port CL:  Relays 17 - 20.  CL = Port C, Low bits.
-    //  - Port CH:  Relays 21 - 24.  CH = Port C, High bits.
     [Flags]
     public enum ERB24_FLAGS {
-        None = 0x0,
-        F00 = 0x1, F01 = 0x2, F02 = 0x4, F03 = 0x8, F04 = 0x10, F05 = 0x20, F06 = 0x40, F07 = 0x80,
-        F08 = 0x100, F09 = 0x200, F10 = 0x400, F11 = 0x800, F12 = 0x1000, F13 = 0x2000, F14 = 0x4000, F15 = 0x8000,
-        F16 = 0x10000, F17 = 0x20000, F18 = 0x40000, F19 = 0x80000, F20 = 0x100000, F21 = 0x200000, F22 = 0x400000, F23 = 0x800000
+        None = 0,
+        F00 = 1 << 00, F01 = 1 << 01, F02 = 1 << 02, F03 = 1 << 03, F04 = 1 << 04, F05 = 1 << 05, F06 = 1 << 06, F07 = 1 << 07,
+        F08 = 1 << 08, F09 = 1 << 09, F10 = 1 << 10, F11 = 1 << 11, F12 = 1 << 12, F13 = 1 << 13, F14 = 1 << 14, F15 = 1 << 15,
+        F16 = 1 << 16, F17 = 1 << 17, F18 = 1 << 18, F19 = 1 << 19, F20 = 1 << 20, F21 = 1 << 21, F22 = 1 << 22, F23 = 1 << 23,
+        PORT_A  = F00 | F01 | F02 | F03 | F04 | F05 | F06 | F07,
+        PORT_B  = F08 | F09 | F10 | F11 | F12 | F13 | F14 | F15,
+        PORT_CL = F16 | F17 | F18 | F19, // Port C low bits.
+        PORT_CH = F20 | F21 | F22 | F23  // Port C high bits.
     }
     [Flags]
     public enum ERB24_PORT_A { None = ERB24_FLAGS.None, R01 = ERB24_FLAGS.F00, R02 = ERB24_FLAGS.F01, R03 = ERB24_FLAGS.F02, R04 = ERB24_FLAGS.F03, R05 = ERB24_FLAGS.F04, R06 = ERB24_FLAGS.F05, R07 = ERB24_FLAGS.F06, R08 = ERB24_FLAGS.F07 }
@@ -65,7 +62,16 @@ namespace TestLibrary.Switching {
         //  - Pull-Up:        Relays are energized at power-up.
         //  - Pull-Down:      Relays are de-energized at power-up.
         //  - https://www.mccdaq.com/PDFs/Manuals/usb-erb24.pdf.
-
+        private static Dictionary<ERB24_RELAYS, ERB24_FLAGS> ERB24_Bits = new Dictionary<ERB24_RELAYS, ERB24_FLAGS>() {
+            { ERB24_RELAYS.R01, ERB24_FLAGS.F00 }, { ERB24_RELAYS.R02, ERB24_FLAGS.F01 }, { ERB24_RELAYS.R03, ERB24_FLAGS.F02 }, { ERB24_RELAYS.R04, ERB24_FLAGS.F03 },
+            { ERB24_RELAYS.R05, ERB24_FLAGS.F04 }, { ERB24_RELAYS.R06, ERB24_FLAGS.F05 }, { ERB24_RELAYS.R07, ERB24_FLAGS.F06 }, { ERB24_RELAYS.R08, ERB24_FLAGS.F07 },
+            { ERB24_RELAYS.R09, ERB24_FLAGS.F08 }, { ERB24_RELAYS.R10, ERB24_FLAGS.F09 }, { ERB24_RELAYS.R11, ERB24_FLAGS.F10 }, { ERB24_RELAYS.R12, ERB24_FLAGS.F11 },
+            { ERB24_RELAYS.R13, ERB24_FLAGS.F12 }, { ERB24_RELAYS.R14, ERB24_FLAGS.F13 }, { ERB24_RELAYS.R15, ERB24_FLAGS.F14 }, { ERB24_RELAYS.R16, ERB24_FLAGS.F15 },
+            { ERB24_RELAYS.R17, ERB24_FLAGS.F16 }, { ERB24_RELAYS.R18, ERB24_FLAGS.F17 }, { ERB24_RELAYS.R19, ERB24_FLAGS.F18 }, { ERB24_RELAYS.R20, ERB24_FLAGS.F19 },
+            { ERB24_RELAYS.R21, ERB24_FLAGS.F20 }, { ERB24_RELAYS.R22, ERB24_FLAGS.F21 }, { ERB24_RELAYS.R23, ERB24_FLAGS.F22 }, { ERB24_RELAYS.R24, ERB24_FLAGS.F23 },
+        };
+        //  - Wish MCC had zero-indexed their Relays, numbering from R0 to R23 instead of R1 to R24.
+        //  - Would've been optimal, as Relays 1 to 24 are controlled by digital port bits that are zero-indexed, from 0 to 23.
         public static Boolean AreERB24BoardsReset() {
             Boolean areERB24BoardsReset = true;
             foreach (ERB24_BOARDS erb24_board in Enum.GetValues(typeof(ERB24_BOARDS))) {
@@ -78,15 +84,15 @@ namespace TestLibrary.Switching {
             return areERB24BoardsReset;
         }
 
-        public static void DeEnergize(ERB24_BOARDS erb24_board) {
+        public static void DeEnergizeERB24(ERB24_BOARDS erb24_board) {
             MccBoard mccBoard = new MccBoard((Int32)erb24_board);
-            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortA, (Int16)ERB24_FLAGS.None);
-            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortB, (Int16)ERB24_FLAGS.None);
-            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortCL, (Int16)ERB24_FLAGS.None);
-            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortCH, (Int16)ERB24_FLAGS.None);
+            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortA,  (UInt16)ERB24_FLAGS.None);
+            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortB,  (UInt16)ERB24_FLAGS.None);
+            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortCL, (UInt16)ERB24_FLAGS.None);
+            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortCH, (UInt16)ERB24_FLAGS.None);
         }
 
-        public static void DeEnergizeAll() { foreach (ERB24_BOARDS erb24_board in Enum.GetValues(typeof(ERB24_BOARDS))) DeEnergize(erb24_board); }
+        public static void DeEnergizeERB24_All() { foreach (ERB24_BOARDS erb24_board in Enum.GetValues(typeof(ERB24_BOARDS))) DeEnergizeERB24(erb24_board); }
 
         public static void SetState((ERB24_BOARDS Board, ERB24_RELAYS Relay) ERB24, FORM_C State) {
             MccBoard mccBoard = new MccBoard((Int32)ERB24.Board);
@@ -119,7 +125,7 @@ namespace TestLibrary.Switching {
             return dataValue;
         }
 
-        private static void DigitalPortWrite(MccBoard mccBoard, DigitalPortType digitalPortType, Int16 dataValue) {
+        private static void DigitalPortWrite(MccBoard mccBoard, DigitalPortType digitalPortType, UInt16 dataValue) {
             ErrorInfo errorInfo = mccBoard.DOut(digitalPortType, dataValue);
             if (errorInfo.Value != ErrorInfo.ErrorCode.NoErrors) MccBoardErrorHandler(mccBoard, errorInfo);
         }
@@ -128,7 +134,7 @@ namespace TestLibrary.Switching {
 
         public static void SetPortAState(ERB24_BOARDS Board, ERB24_PORT_A PortA) {
             MccBoard mccBoard = new MccBoard((Int32)Board);
-            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortA, (Int16)PortA);
+            DigitalPortWrite(mccBoard, DigitalPortType.FirstPortA, (UInt16)PortA);
         }
 
         public static ERB24_PORT_A GetPortAState(ERB24_BOARDS Board) {
