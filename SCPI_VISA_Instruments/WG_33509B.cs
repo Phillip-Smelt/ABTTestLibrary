c88ee21c9bdacd49e60810a3d87345800b6a5d9b
Phillip-Smelt <phillip.smelt@borisch.com>
2023-05-09 14:47:06 -0400

    5/9/2023

--- SCPI_VISA_Instruments/WG_33509B.cs
+++ SCPI_VISA_Instruments/WG_33509B.cs
@@ -23,5 +23,26 @@ namespace ABT.TestSpace.SCPI_VISA_Instruments {
             SCPI99.Initialize(SVI);
             ((Ag33500B_33600A)SVI.Instrument).SCPI.DISPlay.TEXT.CLEar.Command();
         }
+
+        public static void Off(SCPI_VISA_Instrument SVI) {
+            ((Ag33500B_33600A)SVI.Instrument).SCPI.RST.Command();
+            ((Ag33500B_33600A)SVI.Instrument).SCPI.OUTPut.Command(null, false);
+        }
+
+        public static void ApplyWaveformSquare(SCPI_VISA_Instrument SVI, Double FrequencyHz, Double AmplitudeV, Double OffsetV) {
+            ((Ag33500B_33600A)SVI.Instrument).SCPI.SOURce.APPLy.SQUare.Command(null, FrequencyHz, "HZ", AmplitudeV, "V", OffsetV, "V");
+        }
+
+        public static Boolean IsOff(SCPI_VISA_Instrument SVI) {
+            ((Ag33500B_33600A)SVI.Instrument).SCPI.OUTPut.Query(null, out Boolean Output);
+            return Output == false;
+        }
+
+        public static Boolean IsOn(SCPI_VISA_Instrument SVI) { return !IsOff(SVI); }
+        
+        public static String GetWaveform(SCPI_VISA_Instrument SVI) {
+            ((Ag33500B_33600A)SVI.Instrument).SCPI.SOURce.APPLy.Query(null, out String Waveform);
+            return Waveform;
+        }
     }
 }
