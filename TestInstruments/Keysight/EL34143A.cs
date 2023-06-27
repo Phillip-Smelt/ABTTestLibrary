c17d1dd7c7056cbc2b544276a46ca1a526ee424c
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2023-06-27 13:40:13 -0400

    Delete TestInstruments directory

--- TestInstruments/Keysight/EL34143A.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Keysight.KtEL30000; // https://www.keysight.com/us/en/lib/software-detail/driver/el30000a-dc-electronic-loads-ivi-instrument-drivers.html
-
-namespace TestLibrary.TestInstruments.Keysight {
-    // TODO: EL34143A Class.
-    public static class EL34143A {
-        // No SCPI device driver, only IVI.
-    }
-}
