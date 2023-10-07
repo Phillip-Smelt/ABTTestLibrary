8ac1ca0cdd77d89d4453c7c0dc35d9b7478e305e
Phillip-Smelt <phillip.smelt@borisch.com>
2023-10-07 14:49:17 -0400

    10/7/23

--- Logging/SerialNumberDialog.cs
+++ Logging/SerialNumberDialog.cs
@@ -8,7 +8,6 @@
 // PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
 //*********************************************************
-
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
