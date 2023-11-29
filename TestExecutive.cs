3d33e21d5c2522a0b8c516064ff63168ac30702a
Phillip Smelt <phils@borisch.com>
2023-11-29 08:51:12 -0500

    11/29/23

--- TestExecutive.cs
+++ TestExecutive.cs
@@ -542,44 +542,4 @@ namespace ABT.TestSpace.TestExec {
         private Int32 MeasurementResultsCount(Dictionary<String, Measurement> measurements, String eventCode) { return (from measurement in measurements where String.Equals(measurement.Value.Result, eventCode) select measurement).Count(); }
         #endregion Measurements
     }
-
-    public class CancellationException : Exception {
-        // NOTE: Only ever throw CancellationException from TestExecutor, never from TestExecutive.
-        public CancellationException(String message = "") : base(message) { }
-        public const String ClassName = nameof(CancellationException);
-    }
-
-    public class MilliSecondTimer {
-        private readonly Int32 _milliSeconds;
-        private readonly DateTime _start;
-        public MilliSecondTimer(Int32 MilliSeconds) { _milliSeconds = MilliSeconds;  _start = DateTime.Now; }
-
-        public DateTime GetStart() { return _start; }
-        public Int32 GetMilliSecondsTotal() { return _milliSeconds; }
-        public Int32 GetMilliSecondsElapsed() { return (Int32)Math.Round((DateTime.Now - _start).TotalMilliseconds); }
-        public Int32 GetMilliSecondsRemaining() { return Expired() ? 0 : _milliSeconds - GetMilliSecondsElapsed(); }
-        public Boolean Expired() { return GetMilliSecondsElapsed() >= _milliSeconds; }
-        public Boolean NotExpired() { return !Expired(); }
-    }
-
-    public static class EventCodes {
-        public const String CANCEL = "CANCEL";
-        public const String ERROR = "ERROR";
-        public const String FAIL = "FAIL";
-        public const String PASS = "PASS";
-        public const String UNSET = "UNSET";
-
-        public static Color GetColor(String eventCode) {
-            Dictionary<String, Color> codesToColors = new Dictionary<String, Color>() {
-                { EventCodes.CANCEL, Color.Yellow },
-                { EventCodes.ERROR, Color.Aqua },
-                { EventCodes.FAIL, Color.Red },
-                { EventCodes.PASS, Color.Green },
-                { EventCodes.UNSET, Color.Gray }
-            };
-            return codesToColors[eventCode];
-        }
-    }
-
-    public static class Ext{ public static Boolean In<T>(this T value, params T[] values) where T : struct { return values.Contains(value); } }
 }
