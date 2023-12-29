331752061ed859fb18d2296546208681e4c57318
Phillip Smelt <phils@borisch.com>
2023-12-29 12:30:33 -0500

    12/29/23

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace ABT.TestSpace.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Amphenol {
+            get {
+                object obj = ResourceManager.GetObject("Amphenol", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap EmergencyStop {
