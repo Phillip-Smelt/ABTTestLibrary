92b8ea15902e9595a62bf19d7bb3affb17149966
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2024-03-20 09:21:19 -0400

    3/20/24

--- TestExecutive.cs
+++ TestExecutive.cs
@@ -798,7 +798,7 @@ namespace ABT.TestSpace.TestExec {
 
         private void StatusModeUpdate(MODES mode) {
             Dictionary<MODES, Color> ModeColors = new Dictionary<MODES, Color>() {
-                { MODES.Resetting, Color.White },
+                { MODES.Resetting, SystemColors.Control }, // Invisible ink!
                 { MODES.Selecting, Color.Black },
                 { MODES.Running, Color.Green },
                 { MODES.Cancelling, Color.Yellow },
